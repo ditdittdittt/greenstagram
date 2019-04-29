9a9c9cbd3a0ecd3495b74e7e7247f4fb593636d9
ditdittdittt <ditdittdittt@gmail.com>
2019-04-29 16:26:45 +0700

    Initial commit

---
+++ src/app/firebase.ts
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyAeAYJ4qTVZF0RiTTMez22n98Bh2yif-3k",
+  authDomain: "rpl-socialapp.firebaseapp.com",
+  databaseURL: "https://rpl-socialapp.firebaseio.com",
+  projectId: "rpl-socialapp",
+  storageBucket: "rpl-socialapp.appspot.com",
+  messagingSenderId: "758241944772"
+};
+
+export default config
